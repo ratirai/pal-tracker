6aa1c21120b40b2bc72ef02194af053e209878b2
ankit-hsc <luckyandlucy@gmail.com>
2019-08-07 19:44:21 +0530

    jdbc changes

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,8 +1,11 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -11,8 +14,10 @@ public class PalTrackerApplication {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
+    @Autowired
+    private DataSource dataSource;
     @Bean
     public TimeEntryRepository timeEntryRepository() {
-       return new InMemoryTimeEntryRepository();
+       return new JdbcTimeEntryRepository(dataSource);
     }
 }
